e879ac6708262e31465aff2daab8869477235655
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-23 19:04:24 +0530

    Multiple times of flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -8,3 +8,20 @@ then
 else
 	echo "Tail wins"
 fi
+value=0;
+value1=0;
+read -p "Enter number : " n
+for (( i=1; i <= $n; i++ ))
+do
+	num1=$((RANDOM%2))
+	if [ $num1 == 0 ]
+	then
+		value=$(($value+1))
+		echo "Head won $value times"
+	fi
+	if [ $num1 == 1 ]
+	then
+		value1=$(($value1+1))
+		echo "Tail won $value1 times"
+	fi
+done
