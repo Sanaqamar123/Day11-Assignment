d7ed283a2f83262ce8b12eeaf2f936a96e3277cf
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-23 19:44:00 +0530

    Win by how much or tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 num=$((RANDOM%2))
 
@@ -25,3 +25,40 @@ do
 		echo "Tail won $value1 times"
 	fi
 done
+
+for (( i=1; i <= 42; i++ ))
+do
+        num2=$((RANDOM%2))
+        if [ $num2 == 0 ]
+        then
+                value=$(($value+1))
+#                echo "Head won $value times"
+        fi
+        if [ $num2 == 1 ]
+        then
+                value1=$(($value1+1))
+#                echo "Tail won $value1 times"
+        fi
+	if [ $value == 21 ]
+                then
+                        echo "Head wins 21 times"
+	elif [ $value1 == 21 ]
+                then
+                        echo "Tail wins 21 times"
+		break;
+        fi
+done
+if [ $value -eq 21 ] && [ $value1 -eq 21 ]
+then
+	echo "TIE"
+	elif [ $value -eq 21 ] && [ $value1 -lt 21 ]
+	then
+		result=$(($value-$value1))
+		echo "Head won 21 times"
+		echo "Head won by $result times"
+	elif [ $value1 -eq 21 ] && [ $value -lt 21 ]
+	then
+		result1=$(($value1-$value))
+		echo "Tail won 21 times"
+		echo "Tail won by $result1 times"
+fi
