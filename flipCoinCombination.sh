a4793bc2679f63a052e90d223cdc5f5a16edbe6e
Sanaqamar123 <sana.qamr@gmail.com>
2020-09-26 00:17:01 +0530

    Store in dictionary and find percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -11,19 +11,19 @@ fi
 
 declare -A store
 store=()
-value=0;
-value1=0;
-read -p "Enter number" n
+value=0; #value init for how many head occur in multiple flipping of a coin
+value1=0; #value1 init for how many tail occur in multiple flipping of a coin
+read -p "Enter number for multiple flipping of a coin" n
 for (( i = 1; i <= $n; i++ ))
 do
 	num=$((RANDOM%2))
 	if [ $num == 0 ]
 	then
-			num=H
+			num=H;
 			value=$(($value+1))
 			echo "Head"
 	else
-			num=T
+			num=T;
 			value1=$(($value1+1))
 			echo "Tail"
 	fi
@@ -31,8 +31,8 @@ do
 	num1=$value;
 	num2=$value1;
 done
-num3=$num1;
-num4=$num2;
+num3=$num1; #num3=value
+num4=$num2; #num4=value1
 echo "Value store in dictionary" ${store[@]}
 TotalValueInDictionary=${#store[@]}
 PercentageForHead=$(($TotalValueInDictionary/$num3))
